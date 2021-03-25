254445fff354aaf5f1b54754b610b137ced72a6f
XPS <mcleancolinm@gmail.com>
2021-03-25 17:04:41 -0500

    Start writing an asm file manually that translates the basic tests

---
+++ nand2tetris/projects/07/MemoryAccess/StaticTest/manual_compile.asm
@@ -0,0 +1,20 @@
+    
+    // set location of pointer
+    @100
+    D=A
+
+    @SP
+    M=D
+    
+
+    // push constant 111
+    
+    @111
+    D=A
+
+    @SP
+    A=M
+    M=D
+
+    @SP
+    M=M+1

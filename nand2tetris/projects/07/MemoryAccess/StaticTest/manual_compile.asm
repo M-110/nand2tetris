332bc353716a4c1a9bca02c7ebb327ca19b833df
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-03-25 21:42:57 -0500

    Update manualcompiler script

--- nand2tetris/projects/07/MemoryAccess/StaticTest/manual_compile.asm
+++ nand2tetris/projects/07/MemoryAccess/StaticTest/manual_compile.asm
@@ -18,3 +18,66 @@
 
     @SP
     M=M+1
+
+    // push constant 333
+
+    @333
+    D=A
+    
+    @SP
+    A=M
+    M=D
+
+    @SP
+    M=M+1
+
+    // push constant 888
+
+    @888
+    D=A
+    
+    @SP
+    A=M
+    M=D
+
+    @SP
+    M=M+1
+
+    // pop static 8
+   
+    @SP
+    M=M-1
+    A=M
+    D=M
+
+    @static.8
+    M=D
+
+    // pop static 3
+    
+    @SP
+    M=M-1
+    A=M
+    D=M
+
+    @static.3
+    M=D
+    
+    // pop static 1
+    
+    @SP
+    M=M-1
+    A=M
+    D=M
+
+    @static.1
+    M=D
+
+    // push static 3
+
+
+    // push static 1
+
+
+    
+
